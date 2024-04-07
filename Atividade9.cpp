f815ef1c55946c2a71bd16629be1d4615add344e
Paulo Padilha <paulopadilha2302@gmail.com>
2024-04-07 19:56:10 -0300

    Quarto commit

--- Atividade9.cpp
+++ Atividade9.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Função para calcular e imprimir os N termos da sequência de Fibonacci
 void fibonacci(int n) {
     int primeiro = 0, segundo = 1, proximo, i;
 
@@ -22,7 +21,6 @@ void fibonacci(int n) {
 int main() {
     int n;
 
-    // Solicita ao usuário o número de termos a serem impressos
     printf("Digite o numero de termos da sequencia de Fibonacci a serem exibidos: ");
     scanf("%d", &n);
 
@@ -32,7 +30,6 @@ int main() {
         return 1;
     }
 
-    // Chamada da função fibonacci para imprimir os termos
     printf("Sequencia de Fibonacci com %d termos:\n", n);
     fibonacci(n);
 
